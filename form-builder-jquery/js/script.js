2524a5cac0ef7f0c6384966fbc97bc289a5517b7
abhijit kumar Das <abhijit.kumar.das@massoftind.com>
2021-04-06 19:52:39 +0530

    show form builder

--- form-builder-jquery/js/script.js
+++ form-builder-jquery/js/script.js
@@ -1,33 +1,15 @@
-'use strict';
-$(function () {
-    var options = {
-        controlPosition: 'left',
-        controlOrder: [
-            'autocomplete',
-            'button',
-            'checkbox-group',
-            'date',
-            'file',
-            'header',
-            'hidden',
-            'number',
-            'paragraph',
-            'radio-group',
-            'select',
-            'text',
-            'textarea'
-        ],
-        disableFields: ['autocomplete', 'file', 'hidden', 'header', 'number', 'paragraph']
-    }
-    // $('.build-wrap').formBuilder(options);
+jQuery(function ($) {
     var fbEditor = document.getElementById('build-wrap');
-    var formBuilder = $(fbEditor).formBuilder(options);
-    document.getElementById('getJSON').addEventListener('click', function () {
-        console.log(formBuilder.actions.getData('json', true))
+    var formBuilder = $(fbEditor).formBuilder();
+
+    document.getElementById('getXML').addEventListener('click', function () {
+        alert(formBuilder.actions.getData('xml'));
     });
-
-    console.log('jjjjj', $('.save-template').val());
-
-
-
+    document.getElementById('getJSON').addEventListener('click', function () {
+        alert(formBuilder.actions.getData('json'));
+    });
+    document.getElementById('getJS').addEventListener('click', function () {
+        alert('check console');
+        console.log(formBuilder.actions.getData());
+    });
 });

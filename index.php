5593730793ac59e0b8c9946e5d3121e312a2e4f8
Abhijit Kumar Das <NODE-A030@SLEEK.COM>
2021-04-07 10:41:51 +0530

    folder push

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+<script src="https://formbuilder.online/assets/js/form-builder.min.js"></script>
+
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="css/style.css" type="text/css">
+
+<body>
+    <div id="build-wrap"></div>
+    <script src="./js/script.js"></script>
+</body>
+
+</html>
---
+++ js/script.js
@@ -0,0 +1,46 @@
+'use strict';
+$(function () {
+    var options = {
+        controlPosition: 'left',
+        controlOrder: [
+            'autocomplete',
+            'button',
+            'checkbox-group',
+            'date',
+            'file',
+            'header',
+            'hidden',
+            'number',
+            'paragraph',
+            'radio-group',
+            'select',
+            'text',
+            'textarea'
+        ],
+        disableFields: ['autocomplete', 'file', 'hidden', 'header', 'number', 'paragraph']
+    }
+    var fbEditor = document.getElementById('build-wrap');
+    var formBuilder = $(fbEditor).formBuilder(options);
+    setTimeout(() => {
+        document
+        .querySelector(".save-template")
+        .addEventListener("click", (e) => {
+            formBuilder.actions.getData('json', true);
+            console.log('alldataset', formBuilder.actions.getData('json', true));
+
+            $.ajax({
+                type: "POST",
+                url: "<?php echo base_url(); ?>index.php/front/Login",
+                data: {
+                    email: email
+                },
+                success: function(data) {
+                    // var obj = $.parseJSON(data);    
+                    
+                }
+            });
+        });
+    }, 1000);
+
+    
+});
